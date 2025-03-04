cee736ee23427babe4ad089781b470fd547af642
Tag6362 <tag6362@gmail.com>
2025-03-04 20:28:01 +0100

    Basic authentication with Github

--- app/web/src/app/(auth)/login/page.tsx
+++ app/web/src/app/(auth)/login/page.tsx
@@ -1,11 +1,20 @@
 import { LoginForm } from "@/components/forms/login-form"
+import { signIn } from "@/auth"
 
 export default function LoginPage() {
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-3xl">
-        <LoginForm />
-      </div>
-    </div>
+    <form
+    action={async () => {
+      "use server"
+      await signIn("github")
+    }}
+  >
+    <button type="submit">Signin with GitHub</button>
+  </form>
+    // <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+    //   <div className="w-full max-w-sm md:max-w-3xl">
+    //     <LoginForm />
+    //   </div>
+    // </div>
   )
 }
